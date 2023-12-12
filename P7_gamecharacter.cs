aa5860643ffed74d50326b44ea801105fc4968a0
animalpark123 <97274907+animalpark123@users.noreply.github.com>
2023-12-13 00:29:22 +0530

    Add files via upload

---
+++ P7_gamecharacter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Animate : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb2d;   
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector2(moveHorizontal, moveVertical).normalized * speed * Time.deltaTime;
+        rb2d.AddForce(movement);
+    }
+}
---
+++ camerashake.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class cameraShakeController : MonoBehaviour
+{   
+    public Transform cameraTransform = default;
+    private Vector3 _orignalPosOfCam = default;
+    public float shakeFrequency = default;
+    void Start()
+    {
+        _orignalPosOfCam = cameraTransform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            CameraShake();
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            StopShake();
+        }
+    }
+    private void CameraShake()
+    {
+        cameraTransform.position = _orignalPosOfCam + Random.insideUnitSphere *
+        shakeFrequency;
+    }
+    private void StopShake()
+    {
+        cameraTransform.position = _orignalPosOfCam;
+    }
+}
